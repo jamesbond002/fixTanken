a8ebe9d41156706648a70c8bf32a9dbf4418ad6a
jamesbond002 <jamesbond002@yandex.com>
2015-04-12 12:54:33 +0200

    Create plugin.js

---
+++ plugin.js
@@ -0,0 +1,31 @@
+{
+    "moduleId" : "jamesbond002/fixTanken",
+    "title" : "TankFix",
+    "subtitle" : "www.tankfix.de",
+    
+    "backButton" : true,
+    "enableGPS" : true,
+    "zoomControl" : false,
+    "screenLockRot" : false,
+    "reload" : true,
+    
+    "shouldOverrideUrlLoading1" : "http://www.tankfix.de",
+    "shouldOverrideUrlLoading2" : "https://www.tankfix.de",
+
+    "loadUrl" : "http://www.tankfix.de",
+       			
+    "browserLaunchLink" : "http://www.tankfix.de", 
+    
+    "onPageFinishedLoadUrl" : "
+javascript:
+myCon = { locationType:'auto', currentGPS : {lat:#lat#,lon:#lng#} };
+updateConfig(myCon);
+submitIt();
+gMapCenter();
+$('.search').removeClass('active');
+$('.map').addClass('active');
+$('#map-canvas').addClass('active');
+$('#searchboxwrap').removeClass('active');    
+map.setZoom(#zoom#);
+    "
+}
